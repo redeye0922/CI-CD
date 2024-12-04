7bad704c5ca020ce568738c91550138cbc9ddd28
jhjung <redeye0922@naver.com>
2024-12-04 14:16:27 +0900

    Update index.js

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,16 +1,23 @@
-mport { createRouter, createWebHistory } from 'vue-router';
+import {
+    createRouter,
+    createWebHistory
+} from 'vue-router';
 import PostList from '../components/PostList.vue';
 import CreatePost from '../components/CreatePost.vue';
 
-const routes = [
-	  { path: '/', component: PostList },
-	  { path: '/create', component: CreatePost }
+const routes = [{
+        path: '/',
+        component: PostList
+    },
+    {
+        path: '/create',
+        component: CreatePost
+    }
 ];
 
 const router = createRouter({
-	  history: createWebHistory(),
-	  routes
+    history: createWebHistory(),
+    routes
 });
 
 export default router;
-
