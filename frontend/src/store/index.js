875b0ffd52dfb4caf0f3be16c5133f580a98b886
jhjung <redeye0922@naver.com>
2024-12-04 15:51:30 +0900

    Update index.js

--- frontend/src/store/index.js
+++ frontend/src/store/index.js
@@ -1,40 +1,43 @@
-import {
-    createStore
-} from 'vuex';
+import { createStore } from 'vuex';
 
 export default createStore({
-    state: {
-        posts: []
+  state: {
+    posts: []
+  },
+  mutations: {
+    setPosts(state, posts) {
+      state.posts = posts;
     },
-    mutations: {
-        setPosts(state, posts) {
-            state.posts = posts;
+    addPost(state, post) {
+      state.posts.push(post);
+    },
+    removePost(state, postId) {
+      state.posts = state.posts.filter(post => post.id !== postId);
+    }
+  },
+  actions: {
+    async fetchPosts({ commit }) {
+      const response = await fetch('http://localhost:9090/posts');
+      const data = await response.json();
+      commit('setPosts', data);
+    },
+    async createPost({ commit }, post) {
+      const response = await fetch('http://localhost:9090/posts', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json'
         },
-        addPost(state, post) {
-            state.posts.push(post);
-        }
+        body: JSON.stringify(post)
+      });
+      const data = await response.json();
+      commit('addPost', data);
     },
-    actions: {
-        async fetchPosts({
-            commit
-        }) {
-            const response = await fetch('http://localhost:9090/api/posts');
-            const data = await response.json();
-            commit('setPosts', data);
-        },
-        async createPost({
-            commit
-        }, post) {
-            const response = await fetch('http://localhost:9090/api/posts', {
-                method: 'POST',
-                headers: {
-                    'Content-Type': 'application/json'
-                },
-                body: JSON.stringify(post)
-            });
-            const data = await response.json();
-            commit('addPost', data);
-        }
-    },
-    modules: {}
+    async deletePost({ commit }, postId) {
+      await fetch(`http://localhost:9090/posts/${postId}`, {
+        method: 'DELETE'
+      });
+      commit('removePost', postId);
+    }
+  },
+  modules: {}
 });
