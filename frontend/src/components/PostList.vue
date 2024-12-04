266c40e89ed6b28c2cad848c2caeaa2e933c32e4
jhjung <redeye0922@naver.com>
2024-12-04 14:18:29 +0900

    Update PostList.vue

--- frontend/src/components/PostList.vue
+++ frontend/src/components/PostList.vue
@@ -1,22 +1,21 @@
-te>
+<template>
   <div>
-	      <h1>게시판</h1>
-	          <ul>
-			        <li v-for="post in posts" :key="post.id">{{ post.title }}</li>
-				    </ul>
-				      </div>
-				      </template>
+    <h1>게시판</h1>
+    <ul>
+      <li v-for="post in posts" :key="post.id">{{ post.title }}</li>
+    </ul>
+  </div>
+</template>
 
-				      <script>
-					      import { mapState } from 'vuex';
+<script>
+import { mapState } from 'vuex';
 
-					      export default {
-						        computed: {
-								    ...mapState(['posts'])
-								  },
-						        created() {
-								    this.$store.dispatch('fetchPosts');
-								  }
-					      };
-				      </script>
-
+export default {
+  computed: {
+    ...mapState(['posts'])
+  },
+  created() {
+    this.$store.dispatch('fetchPosts');
+  }
+};
+</script>
