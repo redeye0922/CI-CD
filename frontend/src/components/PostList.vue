0e0f5105723386f518e75a7ed08cbb3ba9507786
jhjung <redeye0922@naver.com>
2024-12-04 15:50:55 +0900

    Update PostList.vue

--- frontend/src/components/PostList.vue
+++ frontend/src/components/PostList.vue
@@ -2,9 +2,11 @@
   <div>
     <h1>게시판</h1>
     <ul>
-      <li v-for="post in posts" :key="post.id">{{ post.title }}</li>
+      <li v-for="post in posts" :key="post.id">
+        {{ post.title }}
+        <button @click="confirmDelete(post.id)">삭제</button>
+      </li>
     </ul>
-    <button @click="goToCreatePost">새 게시물 작성</button>
   </div>
 </template>
 
@@ -12,6 +14,11 @@
 import { mapState } from 'vuex';
 
 export default {
+  data() {
+    return {
+      postToDelete: null
+    };
+  },
   computed: {
     ...mapState(['posts'])
   },
@@ -19,8 +26,19 @@ export default {
     this.$store.dispatch('fetchPosts');
   },
   methods: {
-    goToCreatePost() {
-      this.$router.push('/create');
+    confirmDelete(postId) {
+      if (confirm('삭제하시겠습니까?')) {
+        this.deletePost(postId);
+      }
+    },
+    async deletePost(postId) {
+      try {
+        await this.$store.dispatch('deletePost', postId);
+        alert('게시물이 삭제되었습니다.');
+      } catch (error) {
+        console.error("게시물 삭제 중 오류가 발생했습니다:", error);
+        alert('게시물 삭제에 실패했습니다.');
+      }
     }
   }
 };
@@ -35,16 +53,16 @@ li {
 }
 
 button {
-  padding: 10px 20px;
-  background: #4CAF50;
+  padding: 5px 10px;
+  background: #ff4d4d;
   color: white;
   border: none;
   border-radius: 4px;
   cursor: pointer;
-  margin-top: 20px;
+  margin-left: 10px;
 }
 
 button:hover {
-  background: #45a049;
+  background: #ff1a1a;
 }
 </style>
