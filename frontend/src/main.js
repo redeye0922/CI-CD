0297e4b1549833746ad783d64dd3cbd4f101778d
jhjung <redeye0922@naver.com>
2024-12-04 14:15:55 +0900

    Update main.js

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,7 +1,8 @@
-import { createApp } from 'vue';
+import {
+    createApp
+} from 'vue';
 import App from './App.vue';
 import store from './store';
 import router from './router';
 
 createApp(App).use(store).use(router).mount('#app');
-
