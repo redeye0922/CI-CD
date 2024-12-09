98c4c1908046b653f6bcbf51713ba63d571940a2
jhjung <redeye0922@naver.com>
2024-12-09 16:43:12 +0900

    Update vue.config.js

--- frontend/vue.config.js
+++ frontend/vue.config.js
@@ -5,7 +5,7 @@ const {
 module.exports = defineConfig({
     transpileDependencies: true,
     devServer: {
-        port: process.env.VUE_APP_PORT || 3030,
+        port: process.env.VUE_APP_FRONTEND_PORT || 3030,
         host: '0.0.0.0'
     }
 })
